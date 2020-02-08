5a3f43abfdb36468b2c20b566d722b85ec4a79f9
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2020-02-07 23:24:28 -0500

    Changed expelling

--- api/server.js
+++ api/server.js
@@ -5,9 +5,9 @@ const morgan = require("morgan");
 
 const server = express();
 
-const setting = [helmet(), cors(), morgan("dev"), express.json()];
+const settings = [helmet(), cors(), morgan("dev"), express.json()];
 
-server.use(setting);
+server.use(settings);
 
 server.get("/", (req,res) => {
   res.status(200).send({message: "Sever is Live"})
