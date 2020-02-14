6f9ee761cbedd43d8ae1410760e1f69e06036366
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2020-02-13 22:52:21 -0500

    finished auth login and middleware

--- auth/userRoutes.js
+++ auth/userRoutes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secret = require("../config/secret");
 const User = require("./userModel");
 
 router.get("/", (req, res) => {
@@ -7,7 +9,7 @@ router.get("/", (req, res) => {
     .then(user => res.status(200).json(user))
     .catch(err => {
       console.log("does this show");
-      res.status(500).json({ message: "I dont goofed" });
+      res.status(500).json({ message: "I done goofed" });
     });
 });
 
@@ -29,6 +31,32 @@ router.post("/:id/register", (req, res) => {
       );
   }
 });
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  User.findBy({ username })
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ user: user, token: token });
+      } else {
+        res.status(401).json({ message: "Invalid Username/Password" });
+      }
+    })
+    .catch(err => res.status(500).json({ message: "Could not login" }));
+});
+
+const generateToken = user => {
+  const payload = {
+    userID: user.id,
+    username: user.username
+  };
+
+  const options = { expiresIn: "1h" };
+  const token = jwt.sign(payload, secret.jwtSecrets, options);
+  return token;
+};
 
 router.post("/roles", (req, res) => {
   const role = req.body;
