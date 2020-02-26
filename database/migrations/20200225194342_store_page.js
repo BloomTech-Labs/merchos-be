1b0c37f85069680a14395d6b009acbd731693a48
Christopher Ksiazak <cksiazak@gmail.com>
2020-02-25 21:35:46 -0700

    built out migrations for store components and layout

---
+++ database/migrations/20200225194342_store_page.js
@@ -0,0 +1,25 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('store_page', tbl => {
+    tbl.increments();
+
+    tbl
+      .integer('store_id')
+      .unsigned()
+      .references('id')
+      .inTable('store')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+
+    tbl
+      .integer('page_id')
+      .unsigned()
+      .references('id')
+      .inTable('page')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.deleteTableIfExists('store_page');
+};
