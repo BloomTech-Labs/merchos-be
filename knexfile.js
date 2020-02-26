ec0f83d7acfffe416a65ca9ff3349c31483afe3c
Christopher Ksiazak <cksiazak@gmail.com>
2020-02-25 19:25:22 -0700

    initializing branch, slight changes to seeds

--- knexfile.js
+++ knexfile.js
@@ -1,51 +1,53 @@
 // Update with your config settings.
 
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   development: {
-    client: "pg",
-    connection: {
-      host: "127.0.0.1",
-      user: process.env.USER,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE,
-      charset: "utf8"
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    // {
+    //   host: "127.0.0.1",
+    //   user: process.env.USER,
+    //   password: process.env.PASSWORD,
+    //   database: process.env.DATABASE,
+    //   charset: "utf8"
+    // },
     migrations: {
-      directory: "./database/migrations"
+      directory: './database/migrations'
     },
     seeds: {
-      directory: "./database/seeds/dev"
+      directory: './database/seeds'
     },
     useNullAsDefault: true
   },
 
   production: {
-    client: "pg",
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: "./database/migrations"
+      directory: './database/migrations'
     },
     seeds: {
-      directory: "./database/seeds/dev"
+      directory: './database/seeds'
     }
   },
 
   testing: {
-    client: "pg",
-    connection: {
-      host: "127.0.0.1",
-      user: process.env.USER,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE,
-      charset: "utf8"
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    // {
+    //   host: "127.0.0.1",
+    //   user: process.env.USER,
+    //   password: process.env.PASSWORD,
+    //   database: process.env.DATABASE,
+    //   charset: "utf8"
+    // },
     migrations: {
-      directory: "./database/migrations"
+      directory: './database/migrations'
     },
     seeds: {
-      directory: "./database/seeds/dev"
+      directory: './database/seeds'
     },
     useNullAsDefault: true
   }
