539a3aae4a8f6226db8e9ca59e9f628f943849fa
Christopher Ksiazak <cksiazak@gmail.com>
2020-03-02 22:28:34 -0700

    added initial tests for backend routes

--- routes/_test_/storeRouter.spec.js
+++ routes/_test_/storeRouter.spec.js
@@ -58,14 +58,44 @@ describe('Store Router', () => {
       });
     });
   });
+
+  describe('Creates a store', () => {
+    const storeInfo = {
+      store: {
+        store_name: 'testingstore1',
+        store_url: 'teststore123'
+      }
+    };
+    it('returns a status of 201', async () => {
+      const store = await request(server)
+        .post(`/store`)
+        .send(storeInfo);
+      expect(store.status).toBe(201);
+    });
+  });
+
+  describe('updates a store', () => {
+    const storeName = 'merchos_test_store';
+
+    it('responds with 201', async () => {
+      const store = await request(server)
+        .put(`/store/${storeName}`)
+        .send({
+          store_name: 'merchos_test_store',
+          store_url: 'test124'
+        });
+      expect(store.status).toBe(201);
+    });
+  });
+
+  describe('deletes a store', () => {
+    const storeName = 'merchos_test_store';
+
+    it('responds with 202', async () => {
+      const store = await request(server).delete(`/store/${storeName}`);
+      expect(store.status).toBe(202);
+    });
+  });
 });
-
-// get specific store
-
-// create a store
-
-// update a store
-
-// delete a store
 
 // get specific user store
