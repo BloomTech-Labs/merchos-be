29b6185fca8fbc0df5d3b6ac13cd6a6d389fee96
Christopher Ksiazak <cksiazak@gmail.com>
2020-03-11 19:44:32 -0600

    added seeding script to package json, adjusted to use pg instead of postgresql

--- knexfile.js
+++ knexfile.js
@@ -50,17 +50,22 @@ module.exports = {
     useNullAsDefault: true
   },
   test: {
-    client: "postgresql", connection: {
-        database: "merchos-test",
-        user: "postgres",
-        password: "password",
-        port: 5432,
-    }, migrations: {
-        directory: "./database/migrations",
-    }, seeds: {
-        directory: "./database/seeds",
-    }, pool: {
-        min: 2, max: 10,
+    client: 'pg',
+    connection: {
+      database: 'merchos-test',
+      user: 'postgres',
+      password: 'password',
+      port: 5432
     },
-}
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
+  }
 };
