b5b80a462dfcad21c501203131bb61ca177c233b
Christopher Ksiazak <cksiazak@gmail.com>
2020-04-29 16:35:57 -0600

    fixed failing tests

--- database/seeds/05-page.js
+++ database/seeds/05-page.js
@@ -1,16 +1,16 @@
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex("page")
+  return knex('page')
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex("page").insert([
+      return knex('page').insert([
         {
-          layout: `[{ "minW": 12, "maxW": 12, "minH": 2, "maxH": 2, "w": 12, "h": 2, "x": 0, "y": 3, "i": "0" }]`,
-          content: `[
-            {"style": {},"content": {"message": "stuff"},"contentType": "banner","id": "banner-${Date.now().toPrecision()}"}
-          ]`
-        }
-      ]);
-    });
-};
+          content:
+            "[{'style': {},'content': {'message': 'stuff'},'contentType':'banner','id': 'banner-1588199433000'}]",
+          layout:
+            "[{ 'minW': 12, 'maxW': 12, 'minH': 2, 'maxH': 2, 'w': 12, 'h': 2, 'x': 0, 'y': 3, 'i': '0' }]",
+        },
+      ])
+    })
+}
